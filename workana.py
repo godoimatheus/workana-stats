c1eb956d8f5739286f4edbb27eff2d77f71babee
Matheus Godoi <matheus.godoi@live.com>
2023-03-23 14:05:32 -0300

    add filtro de forma de pagamento

--- workana.py
+++ workana.py
@@ -5,6 +5,7 @@ import pandas as pd
 import math
 import json
 from datetime import datetime
+import sqlite3
 
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                          "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"}
@@ -14,9 +15,25 @@ fmt_search = search.replace(' ', '+')
 data_completa = datetime.now()
 data_fmt = data_completa.strftime("%d-%m-%Y_%H-%M-%S")
 dic_vagas = {'titulo': [], 'valor': [], 'data': [], 'skill': [], 'pais': []}
+# bd = sqlite3.connect(f'{search}_{data_fmt}.db')
+# cursor = bd.cursor()
+# cursor.execute(f"CREATE TABLE vagas (titulo text, valores text, data text, pais text)")
+forma_pag = int(input('Escolha a forma de pagamento:'
+                      '\n1 - Todas as formas'
+                      '\n2 - Pagamento fixo'
+                      '\n3 - Pagamento por hora'
+                      '\nSua opção: '))
+if forma_pag == 1:
+    agreement = f'agreement='
+elif forma_pag == 2:
+    agreement = f'agreement=fixed'
+elif forma_pag == 3:
+    agreement = f'agreement=hourly'
+else:
+    print('Opção inválida')
 for i in range(1, 51):
-    url_pag = f'https://www.workana.com/jobs?language=pt&query={fmt_search}&page={i}'
     print(f'Página {i}')
+    url_pag = f'https://www.workana.com/jobs?{agreement}&language=pt&query={fmt_search}&page={i}'
     site = requests.get(url_pag, headers=headers)
     soup = BeautifulSoup(site.content, 'html.parser')
     vagas = soup.find_all('div', class_='project-item')
@@ -49,6 +66,7 @@ for i in range(1, 51):
         dic_vagas['skill'].append(lista_skill)
         dic_vagas['pais'].append(pais)
         print()
-
+        # cursor.execute(f"INSERT INTO vagas VALUES('{titulo}', '{valor}', '{data}', '{pais}')")
+        # bd.commit()
 df = pd.DataFrame(dic_vagas)
 df.to_csv(f'{search}_{data_fmt}.csv', encoding='utf-8', sep=';')
