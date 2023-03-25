26872777ffdb3f375085544f8d2783b39fa7de6e
Matheus Godoi <matheus.godoi@live.com>
2023-03-25 15:29:18 -0300

    correcao do banco de dados e de vagas sem valores

--- workana.py
+++ workana.py
@@ -15,9 +15,9 @@ fmt_search = search.replace(' ', '+')
 data_completa = datetime.now()
 data_fmt = data_completa.strftime("%d-%m-%Y_%H-%M-%S")
 dic_vagas = {'titulo': [], 'valorMin': [], 'valorMax': [], 'data': [], 'skill': [], 'pais': []}
-# bd = sqlite3.connect(f'{search}_{data_fmt}.db')
-# cursor = bd.cursor()
-# cursor.execute(f"CREATE TABLE vagas (titulo text, valores text, data text, skills text, pais text)")
+bd = sqlite3.connect(f'{search}_{data_fmt}.db')
+cursor = bd.cursor()
+cursor.execute(f"CREATE TABLE vagas (titulo text, valor_min real, valor_max real, data text, skills text, pais text)")
 forma_pag = int(input('Escolha a forma de pagamento:'
                       '\n1 - Todas as formas'
                       '\n2 - Pagamento fixo'
@@ -44,16 +44,22 @@ for i in range(1, 51):
         titulo = vaga.find('span').get('title')
         print(titulo)
         valor = vaga.find(class_='values').get_text()
-        print(valor)
-        valores = re.findall(r'\d+(?:\.\d+)?', valor)
-        # print(valores)
-        val_min = valores[0]
-        if len(valores) > 1:
-            val_max = valores[1]
+        # print(valor)
+        valor_sem_ponto = valor.replace('.', '')
+        valores = re.findall(r'\d+(?:\.\d+)?', valor_sem_ponto)
+        print(valor_sem_ponto)
+        print(len(valores))
+        if len(valores) == 0:
+            val_min = valor_sem_ponto
+            val_max = valor_sem_ponto  # ou valor sem ponto
         else:
-            val_max = valores[0]
-        print(val_min)
-        print(val_max)
+            val_min = valores[0]
+            if len(valores) > 1:
+                val_max = valores[1]
+            else:
+                val_max = valores[0]
+        # print(val_min)
+        # print(val_max)
         data = vaga.find(class_='date').get('title')
         print(data)
         lista_skill = []
@@ -64,9 +70,11 @@ for i in range(1, 51):
             for s in sk:
                 anchor_text = s['anchorText']
                 lista_skill.append(anchor_text)
+            skills = ', '.join(lista_skill)
             print(lista_skill)
         else:
-            lista_skill.append('Sem skills')
+            skills = 'Sem skills'
+            lista_skill.append(skills)
         pais = vaga.find(class_='country-name').get_text().strip()
         print(pais)
         dic_vagas['titulo'].append(titulo)
@@ -76,7 +84,8 @@ for i in range(1, 51):
         dic_vagas['skill'].append(lista_skill)
         dic_vagas['pais'].append(pais)
         print()
-        # cursor.execute(f"INSERT INTO vagas (titulo, valores, data, pais) VALUES('{titulo}', '{valor}', '{data}', '{pais}')")
-        # bd.commit()
+        cursor.execute(f"INSERT INTO vagas (titulo, valor_min, valor_max, data, skills, pais) VALUES("
+                       f"'{titulo}', '{val_min}', '{val_max}', '{data}', '{skills}', '{pais}')")
+        bd.commit()
 df = pd.DataFrame(dic_vagas)
-df.to_csv(f'{search}_{data_fmt}.csv', encoding='utf-8', sep=';')
+df.to_csv(f'{search}_{data_fmt}.csv', encoding='utf-8', sep=';', index=False)
