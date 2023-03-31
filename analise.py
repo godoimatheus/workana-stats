5dc26b798b493a68f872ab5229fe8ea2c77d5ce7
Matheus Godoi <matheus.godoi@live.com>
2023-03-31 15:36:19 -0300

    melhorias nas datas e pequenas correcoes

---
+++ analise.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('vagas.csv', sep=';')
+print(df)
--- workana.py
+++ workana.py
@@ -13,14 +13,14 @@ search = str(input('Nome da vaga: ')).strip()
 fmt_search = search.replace(' ', '+')
 full_date = datetime.now()
 # fmt_date = full_date.strftime("%d-%m-%Y_%H-%M-%S")  # data para csv
-fmt_date = full_date.strftime("%Y-%m-%d %H:%M:%S")
+fmt_date = full_date.strftime("%Y-%m-%d %H:%M")
 dict_jobs = {'titulo': [], 'valorMin': [], 'valorMax': [], 'formaPag': [],
              'data': [], 'skill': [], 'pais': []}
 # bd = sqlite3.connect(f'{search}_{fmt_date}.db')
 db = sqlite3.connect('sqlite.db')
 cursor = db.cursor()
 cursor.execute(f"CREATE TABLE IF NOT EXISTS vagas ("
-               f"titulo text UNIQUE, valor_min real, valor_max real, forma_pag text, "
+               f"titulo text UNIQUE, valor_min integer, valor_max integer, forma_pag text, "
                f"data text, skills text, pais text, consulta text)")
 payment = int(input('Escolha a forma de pagamento:'
                     '\n1 - Todas as formas'
@@ -37,7 +37,7 @@ else:
     print('Opção inválida')
 for i in range(1, 51):
     print(f'Página {i}')
-    url_pag = f'https://www.workana.com/jobs?{agreement}&language=xx&query={fmt_search}&page={i}'
+    url_pag = f'https://www.workana.com/en/jobs?{agreement}&language=xx&query={fmt_search}&page={i}'
     site = requests.get(url_pag, headers=headers)
     soup = BeautifulSoup(site.content, 'html.parser')
     jobs = soup.find_all('div', class_='project-item')
@@ -47,14 +47,14 @@ for i in range(1, 51):
     for job in jobs:
         title = job.find('span').get('title').replace('"', "'")
         print(title)
-        value_text = job.find(class_='values').get_text().replace('.', '')
+        value_text = job.find(class_='values').get_text().replace(',', '')
         # value_text = value_text.replace('.', '')
         values = re.findall(r'\d+(?:\.\d+)?', value_text)
         print(value_text)
-        if 'hora' in value_text:
-            payment_method = 'Hora'
+        if 'hour' in value_text:
+            payment_method = 'Hourly'
         else:
-            payment_method = 'Fixo'
+            payment_method = 'Fixed'
         if len(values) == 0:
             val_min = value_text
             val_max = value_text  # ou valor sem ponto
@@ -64,8 +64,11 @@ for i in range(1, 51):
                 val_max = values[1]
             else:
                 val_max = values[0]
-        date = job.find(class_='date').get('title')
-        print(date)
+        tag_date = job.find(class_='date').get('title')
+        print(tag_date)
+        date = datetime.strptime(tag_date, "%B %d, %Y %H:%M")
+        date_bd = date.strftime("%Y-%m-%d %H:%M")
+        print(date_bd)
         skills_list = []
         tag_label = job.find_all('label-expander')
         if len(tag_label) > 0:
@@ -85,7 +88,7 @@ for i in range(1, 51):
         dict_jobs['valorMin'].append(val_min)
         dict_jobs['valorMax'].append(val_max)
         dict_jobs['formaPag'].append(payment_method)
-        dict_jobs['data'].append(date)
+        dict_jobs['data'].append(tag_date)
         dict_jobs['skill'].append(skills_list)
         dict_jobs['pais'].append(country)
         print()
@@ -93,7 +96,7 @@ for i in range(1, 51):
                        f'titulo, valor_min, valor_max, forma_Pag, data, skills, pais, consulta) '
                        f'VALUES('
                        f'"{title}", "{val_min}", "{val_max}", "{payment_method}", '
-                       f'"{date}", "{skill_text}", "{country}", "{fmt_date}")')
+                       f'"{date_bd}", "{skill_text}", "{country}", "{fmt_date}")')
         db.commit()
 # df = pd.DataFrame(dict_jobs)
 # df.to_csv(f'{search}.csv', encoding='utf-8', sep=';', index=False)
