e2fa65e9b0c9dfb5d902ebd53d9f28fcccd810d9
Matheus Godoi <matheus.godoi@live.com>
2023-04-05 19:43:35 -0300

    analise das vagas por paises e skills

--- analise.py
+++ analise.py
@@ -3,38 +3,53 @@ from pymongo import MongoClient
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# conectar ao mongo
 client = MongoClient('localhost', 27017)
 db = client['workana']
 collection = db['vagas']
+
+# converter para dataframe
 cursor = collection.find()
 df = pd.DataFrame(list(cursor))
-print(df.columns)
-print(df)
 
-print()
-nomes_quantidade = {}
-nomes_unicos = collection.distinct('skills')
-for nome in nomes_unicos:
-    quantidade = collection.count_documents({'skills': nome})
-    nomes_quantidade[nome] = quantidade
-    # print(f'{nome}: {quantidade}')
-print(nomes_quantidade)
+# calculo da media e criar nova coluna
+df['media'] = (df['valor_min'] + df['valor_max']) / 2
 
+# quantidade de vagas por skill
+df_skills = df.copy()
+df_skills = df_skills.drop(['_id', 'valor_min', 'valor_max'], axis=1)
+df_skills = df_skills.explode('skills')
+tecnologias_vagas = df_skills.groupby('skills')['titulo'].count().sort_values(ascending=False)
+print(tecnologias_vagas.head(10))
 
-'''dicionario_ordenado = dict(sorted(nomes_quantidade.items(), key=lambda item: item[1], reverse=True))
-plt.bar(dicionario_ordenado.keys(), dicionario_ordenado.values())
-plt.show()
+# skills mais bem pagas geral
+tecnologias_pagas = df_skills.groupby('skills')['media'].mean().sort_values(ascending=False)
+print(tecnologias_pagas.head(10))
 
+# quantas vagas por pais
+paises_vagas = df.groupby('pais')['titulo'].count().sort_values(ascending=False)
+print(paises_vagas.head(10))
 
-# ordene o dicionário pelos valores em ordem decrescente
-nomes_quantidade_ordenado = dict(sorted(nomes_quantidade.items(), key=lambda item: item[1], reverse=True))
+# valor geral medio por pais
+paises_maiores_sal = df.groupby('pais')['media'].mean().sort_values(ascending=False)
+print(paises_maiores_sal.head(10))
 
-# pegue as 10 primeiras chaves e valores
-top_10 = dict(list(nomes_quantidade_ordenado.items())[:10])
+# media por pagamento fixo por pais
+pais_fixo = df[['pais', 'media']].loc[df['forma_pag'] == 'Fixed']
+media_pais_fixo = pais_fixo.groupby('pais')['media'].mean().sort_values(ascending=False)
+print(media_pais_fixo.head(10))
 
-# plote um gráfico de barras das 10 principais habilidades
-plt.bar(top_10.keys(), top_10.values())
-plt.xlabel('Habilidade')
-plt.ylabel('Quantidade')
-plt.title('10 principais habilidades')
-plt.show()'''
+# media por pagamento hora de cada pais
+pais_hora = df[['pais', 'media']].loc[df['forma_pag'] == 'Hourly']
+media_pais_hora = pais_hora.groupby('pais')['media'].mean().sort_values(ascending=False)
+print(media_pais_hora.head(10))
+
+# media por pagamento fixo por skills
+skills_fixo = df_skills[['skills', 'media']].loc[df_skills['forma_pag'] == 'Fixed']
+media_skills_fixo = skills_fixo.groupby('skills')['media'].mean().sort_values(ascending=False)
+print(media_skills_fixo.head(10))
+
+# media por pagamento hora por skills
+skills_hora = df_skills[['skills', 'media']].loc[df_skills['forma_pag'] == 'Hourly']
+media_skills_hora = skills_hora.groupby('skills')['media'].mean().sort_values(ascending=False)
+print(media_skills_hora.head(10))
--- workana_mongodb.py
+++ workana_mongodb.py
@@ -14,6 +14,32 @@ client = MongoClient('localhost', 27017)
 db = client['workana']
 collection = db['vagas']
 db['vagas'].create_index([("titulo", pymongo.ASCENDING)], unique=True)
+
+nomes_unicos = collection.distinct('skills')
+print(nomes_unicos)
+paises = ['AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AS',
+          'AT', 'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG',
+          'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT',
+          'BV', 'BW', 'BY', 'BZ', 'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI',
+          'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CW', 'CX', 'CY',
+          'CZ', 'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ', 'EC', 'EE', 'EG', 'EH',
+          'ER', 'ES', 'ET', 'EU', 'FI', 'FJ', 'FK', 'FM', 'FO', 'FR', 'GA',
+          'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP',
+          'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY', 'HK', 'HM', 'HN', 'HR',
+          'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS',
+          'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN',
+          'KP', 'KR', 'KW', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR',
+          'LS', 'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG',
+          'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT',
+          'MU', 'MV', 'MW', 'MX', 'MY', 'MZ', 'NA', 'NC', 'NE', 'NF', 'NG',
+          'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ', 'OM', 'PA', 'PE', 'PF',
+          'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY',
+          'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD', 'SE',
+          'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS',
+          'ST', 'SV', 'SX', 'SY', 'SZ', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ',
+          'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ', 'UA',
+          'UG', 'UM', 'US', 'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN',
+          'VU', 'WF', 'WS', 'YE', 'YT', 'ZA', 'ZM', 'ZW']
 search = str(input('Nome da vaga: ')).strip()
 fmt_search = search.replace(' ', '+')
 full_date = datetime.now()
@@ -31,68 +57,75 @@ elif payment == 3:
     agreement = f'agreement=hourly'
 else:
     print('Opção inválida')
-for i in range(1, 51):
-    print(f'Página {i}')
-    # categoria
-    url_pag = f'https://www.workana.com/en/jobs?category=it-programming&{agreement}&language=xx&query={fmt_search}&page={i}'
-    site = requests.get(url_pag, headers=headers)
-    soup = BeautifulSoup(site.content, 'html.parser')
-    jobs = soup.find_all('div', class_='project-item')
-    if len(jobs) == 0:
-        print('Sem resultados encontrados')
-        break
-    for job in jobs:
-        title = job.find('span').get('title').replace('"', "'")
-        print(title)
-        value_text = job.find(class_='values').get_text().replace(',', '')
-        values = re.findall(r'\d+(?:\.\d+)?', value_text)
-        print(value_text)
-        if 'hour' in value_text:
-            payment_method = 'Hourly'
-        else:
-            payment_method = 'Fixed'
-        if len(values) == 0:
-            val_min = value_text
-            val_max = value_text
-        else:
-            val_min = values[0]
-            if len(values) > 1:
-                val_max = values[1]
+# paises
+# for pais in paises:
+# url_pag = f'https://www.workana.com/en/jobs?country={pais}&page={i}'
+
+# pesquisa por skills
+for nomes in nomes_unicos:
+    for i in range(1, 51):
+        print(f'Página {i}')
+        url_pag = f'https://www.workana.com/en/jobs?&language=xx&query={nomes}&page={i}'
+        # pesquisas
+        # url_pag = f'https://www.workana.com/en/jobs?{agreement}&language=xx&query={fmt_search}&page={i}'
+        site = requests.get(url_pag, headers=headers)
+        soup = BeautifulSoup(site.content, 'html.parser')
+        jobs = soup.find_all('div', class_='project-item')
+        if len(jobs) == 0:
+            print('Sem resultados encontrados')
+            break
+        for job in jobs:
+            title = job.find('span').get('title').replace('"', "'")
+            print(title)
+            value_text = job.find(class_='values').get_text().replace(',', '')
+            values = re.findall(r'\d+(?:\.\d+)?', value_text)
+            print(value_text)
+            if 'hour' in value_text:
+                payment_method = 'Hourly'
             else:
-                val_max = values[0]
-        tag_date = job.find(class_='date').get('title')
-        date = datetime.strptime(tag_date, "%B %d, %Y %H:%M")
-        print(date)
-        # date_bd = date.strftime("%Y-%m-%d %H:%M")
-        # print(date_bd)
-        skills_list = []
-        tag_label = job.find_all('label-expander')
-        if len(tag_label) > 0:
-            expand = tag_label[0][":to-expand"]
-            skills = json.loads(expand)
-            for skill in skills:
-                anchor_text = skill['anchorText']
-                skills_list.append(anchor_text)
-            skill_text = ', '.join(skills_list)
-            print(skills_list)
-        else:
-            skill_text = 'Sem skills'
-            skills_list.append(skill_text)
-        country = job.find(class_='country-name').get_text().strip()
-        print(country)
-        try:
-            db['vagas'].insert_one(
-                {
-                    'titulo': title,
-                    'valor_min': int(val_min),
-                    'valor_max': int(val_max),
-                    'forma_pag': payment_method,
-                    'data_vaga': date,
-                    'skills': skills_list,
-                    'pais': country,
-                    'consulta': full_date
-                }
-            )
-        except PyMongoError as erro:
-            print(erro)
-        print()
+                payment_method = 'Fixed'
+            if len(values) == 0:
+                val_min = 0  # value_text
+                val_max = 0  # value_text
+            else:
+                val_min = values[0]
+                if len(values) > 1:
+                    val_max = values[1]
+                else:
+                    val_max = values[0]
+            tag_date = job.find(class_='date').get('title')
+            date = datetime.strptime(tag_date, "%B %d, %Y %H:%M")
+            print(date)
+            # date_bd = date.strftime("%Y-%m-%d %H:%M")
+            # print(date_bd)
+            skills_list = []
+            tag_label = job.find_all('label-expander')
+            if len(tag_label) > 0:
+                expand = tag_label[0][":to-expand"]
+                skills = json.loads(expand)
+                for skill in skills:
+                    anchor_text = skill['anchorText']
+                    skills_list.append(anchor_text)
+                skill_text = ', '.join(skills_list)
+                print(skills_list)
+            else:
+                skill_text = 'Sem skills'
+                skills_list.append(skill_text)
+            country = job.find(class_='country-name').get_text().strip()
+            print(country)
+            try:
+                db['vagas'].insert_one(
+                    {
+                        'titulo': title,
+                        'valor_min': int(val_min),
+                        'valor_max': int(val_max),
+                        'forma_pag': payment_method,
+                        'data_vaga': date,
+                        'skills': skills_list,
+                        'pais': country,
+                        'consulta': full_date
+                    }
+                )
+            except PyMongoError as erro:
+                print(erro)
+            print()
