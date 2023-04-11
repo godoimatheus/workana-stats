574cebb9565df1b5ec255824dac0744b5612b416
Matheus Godoi <matheus.godoi@live.com>
2023-04-11 12:10:57 -0300

    input usuario pais e skills

--- analise.py
+++ analise.py
@@ -3,6 +3,8 @@ from pymongo import MongoClient
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from collections import Counter
+
 # conectar ao mongo
 client = MongoClient('localhost', 27017)
 db = client['workana']
@@ -70,59 +72,16 @@ print('Maiores médias de pagamentos por hora por skills:')
 print(media_skills_hora.head(10))
 print()
 
-# input usuario escolher pais
-print('Input do usuário país')
-# usuario_pais = input('País: ').strip()
-usuario_pais = 'Brazil'
-print(usuario_pais)
-print(f'Quantidade de vagas: {paises_vagas[usuario_pais]}')
-print(f'Média fixo: {media_pais_fixo[usuario_pais]}')
-print(f'Média por hora: {media_pais_hora[usuario_pais]}')
+# anos com mais vagas
+vagas_ano = df.groupby(df['data_vaga'].dt.strftime('%Y'))['_id'].count().sort_values(ascending=False)
+print('Anos com mais vagas')
+print(vagas_ano.head(10))
 print()
-# top de vagas por pais
-top_skills_pais = df_skills.groupby(['skills', 'pais'])['_id'].count().sort_values(ascending=False)
-top10_pais = top_skills_pais.loc(axis=0)[:, usuario_pais]
-print(f'Top 10 de skills do {usuario_pais}')
-print(top10_pais.head(10))
-print()
-
-# skills mais bem pagas por pais
-pag_skills_pais = df_skills.groupby(['skills', 'pais'])['media'].mean().sort_values(ascending=False)
-top10_sk_pais = pag_skills_pais.loc(axis=0)[:, usuario_pais]
-print(f'Top 10 de skills mais bem pagas do {usuario_pais}')
-print(top10_sk_pais.head(10))
-print()
-# pesquisa por skills
-print('Input do usuário skill')
-# usuario_skill = input('Skill: ').strip()
-usuario_skill = 'Python'
-print(usuario_skill)
-print(f'Quantidade de vagas: {tecnologias_vagas[usuario_skill]}')
-print(f'Média de pagamento: {tecnologias_pagas[usuario_skill]}')
-print()
-print(f'Top 10 de países com mais vagas de {usuario_skill}')
-print(top_skills_pais[usuario_skill].head(10))
-print()
-
-# gráfico definido por quantidade de skills
-media_pais_fixo = pais_fixo.groupby('pais')['media'].agg(['mean', 'count'])
-media_pais_fixo = media_pais_fixo.loc[media_pais_fixo['count'] >= 100].sort_values(by='mean', ascending=False).head(10)
-
-'''sns.set()
-plt.bar(media_pais_fixo.index, media_pais_fixo['mean'])
-plt.xticks(rotation=45)
-plt.show()'''
 
 # meses com mais vagas
 vagas_mes = df.groupby(df['data_vaga'].dt.strftime('%Y-%m'))['_id'].count().sort_values(ascending=False)
 print('Meses com mais vagas')
 print(vagas_mes.head(10))
-print()
-
-# anos com mais vagas
-vagas_ano = df.groupby(df['data_vaga'].dt.strftime('%Y'))['_id'].count().sort_values(ascending=False)
-print('Anos com mais vagas')
-print(vagas_ano.head(10))
 print()
 
 # vagas dia
@@ -131,3 +90,72 @@ print('Dias com mais vagas')
 print(vagas_dia.head(10))
 print()
 
+# input usuario escolher pais
+paises_nomes = collection.distinct('pais')
+print('Input do usuário país')
+usuario_pais = input('País: ').strip()
+for pais in paises_nomes:
+    if usuario_pais.lower() == pais.lower():
+        print(pais)
+        usuario_pais = pais
+
+        print(f'Quantidade de vagas: {paises_vagas[usuario_pais]}')
+        print(f'Média fixo: {media_pais_fixo[usuario_pais]}')
+        print(f'Média por hora: {media_pais_hora[usuario_pais]}')
+        print()
+
+        # top de vagas por pais
+        top_skills_pais = df_skills.groupby(['skills', 'pais'])['_id'].count().sort_values(ascending=False)
+        top10_pais = top_skills_pais.loc(axis=0)[:, usuario_pais]
+        print(f'Top 10 de skills do {usuario_pais}')
+        print(top10_pais.head(10))
+        print()
+
+        # skills mais bem pagas por pais
+        pag_skills_pais = df_skills.groupby(['skills', 'pais'])['media'].mean().sort_values(ascending=False)
+        top10_sk_pais = pag_skills_pais.loc(axis=0)[:, usuario_pais]
+        print(f'Top 10 de skills mais bem pagas do {usuario_pais}')
+        print(top10_sk_pais.head(10))
+        print()
+
+        break
+else:
+    print('Não encontrado')
+
+# pesquisa por skills
+skills_nomes = collection.distinct('skills')
+print('Input do usuário skill')
+usuario_skill = input('Skill: ').strip()
+for skill in skills_nomes:
+    if usuario_skill.lower() == skill.lower():
+        print(skill)
+        usuario_skill = skill
+
+        print(f'Quantidade de vagas: {tecnologias_vagas[usuario_skill]}')
+        print(f'Média de pagamento: {tecnologias_pagas[usuario_skill]}')
+        print()
+        print(f'Top 10 de países com mais vagas de {usuario_skill}')
+        print(top_skills_pais[usuario_skill].head(10))
+        print()
+
+        # skills relacionadas
+        print(f'Skills relacionadas a {usuario_skill}')
+        skills_relacionadas = df[df['skills'].apply(lambda x: usuario_skill in x)]
+        counter = Counter()
+        for skills in skills_relacionadas['skills']:
+            counter.update(skills)
+        counter.pop(usuario_skill)
+        for k, v in counter.most_common()[:11]:
+            print(f'{k}: {v}')
+
+        break
+else:
+    print('Não encontrado')
+
+# gráfico definido por quantidade de skills
+media_pais_fixo = pais_fixo.groupby('pais')['media'].agg(['mean', 'count'])
+media_pais_fixo = media_pais_fixo.loc[media_pais_fixo['count'] >= 100].sort_values(by='mean', ascending=False).head(10)
+sns.set()
+plt.bar(media_pais_fixo.index, media_pais_fixo['mean'])
+plt.xticks(rotation=45)
+plt.show()
