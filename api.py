a5a3671d5cbcfe67b5811ff31ceafad0734c00b1
Matheus Godoi <matheus.godoi@live.com>
2023-04-19 14:38:26 -0300

    criando branch api

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<p>Hello, World!</p>' \
+           'ok'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
